38961ae786665d0f0492fff9971d33edffed8926
Logon PF Fiap <logonpflocal@fiap.com.br>
2021-12-03 12:45:53 -0300

    Continuação da aula

--- app/src/main/java/com/example/materialdesign/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.materialdesign
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.example.materialdesign.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.nextButton.setOnClickListener { abrirTela() }
+    }
+    fun abrirTela(){
+        val minhaTela = Intent(this,Activity2::class.java)
+        startActivity(minhaTela)
     }
 }
